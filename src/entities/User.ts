434eb7b0fac83b34c19babe48163f7903af903f4
alswnd <alswnd0427@gmail.com>
2020-07-13 20:30:35 +0900

    Create User table

---
+++ src/entities/User.ts
@@ -0,0 +1,81 @@
+// import { IsEmail } from "class-validator";
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
+import bcrypt from "bcrypt";
+
+const BCRYPT_ROUNDS = 10;
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "text" })
+  firstName: string;
+
+  @Column({ type: "text" })
+  lastName: string;
+
+  @Column({ type: "text" })
+  profilePhotoUrl: string;
+
+  @Column({ type: "text" })
+  phoneNumber: string;
+
+  @Column({ type: "boolean", default: false })
+  verifiedPhoneNumber: string;
+
+  @Column({ type: "text" })
+  gender: string;
+
+  @Column({ type: "text" })
+  birthDate: string;
+
+  @Column({ type: "text" })
+  job: string;
+
+  @Column({ type: "text" })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  /**
+   * Make User's full name.
+   *
+   * @returns {string} User's full name
+   */
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUNDS);
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if (this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  }
+
+  public comparePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+}
+
+export default User;
