c628c72482b636a937de86a87729e78767e83c84
alswnd <alswnd0427@gmail.com>
2020-07-13 20:43:03 +0900

    Typo

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type GetUserProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype Query {\n  GetUserProfile: GetUserProfileResponse!\n}\n\ntype User {\n  id: Int!\n  fistName: String!\n  lastName: String!\n  fullName: String\n  profilePhotoUrl: String\n  phoneNumber: String!\n  verifiedPhoneNumber: Boolean!\n  gender: String!\n  birthDate: String!\n  job: String!\n  password: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignUpResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Mutation {\n  SignUp(fistName: String!, lastName: String!, profilePhotoUrl: String, phoneNumber: String!, gender: String!, birthDate: String!, job: String!, password: String!): SignUpResponse!\n}\n"];
+export const typeDefs = ["type GetUserProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype Query {\n  GetUserProfile: GetUserProfileResponse!\n}\n\ntype User {\n  id: Int!\n  firstName: String!\n  lastName: String!\n  fullName: String\n  profilePhotoUrl: String\n  phoneNumber: String!\n  verifiedPhoneNumber: Boolean!\n  gender: String!\n  birthDate: String!\n  job: String!\n  password: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignUpResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Mutation {\n  SignUp(firstName: String!, lastName: String!, profilePhotoUrl: String, phoneNumber: String!, gender: String!, birthDate: String!, job: String!, password: String!): SignUpResponse!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -13,7 +13,7 @@ export interface GetUserProfileResponse {
 
 export interface User {
   id: number;
-  fistName: string;
+  firstName: string;
   lastName: string;
   fullName: string | null;
   profilePhotoUrl: string | null;
@@ -32,7 +32,7 @@ export interface Mutation {
 }
 
 export interface SignUpMutationArgs {
-  fistName: string;
+  firstName: string;
   lastName: string;
   profilePhotoUrl: string | null;
   phoneNumber: string;
