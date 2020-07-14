6896c1ecc1c4afd9d633939868363cc836a5dbb8
alswnd <alswnd0427@gmail.com>
2020-07-15 06:01:19 +0900

    Create new Interest

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type CompletPhoneNumberVerificationResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  CompletPhoneNumberVerification(phoneNumber: String!, key: String!): CompletPhoneNumberVerificationResponse!\n  SignUp(firstName: String!, lastName: String!, profilePhotoUrl: String!, phoneNumber: String!, gender: String!, birthDate: String!, job: String!, password: String!): SignUpResponse!\n  StartPhoneVerification(phoneNumber: String!): StartPhoneVerificationResponse!\n}\n\ntype GetUserProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype Query {\n  GetUserProfile: GetUserProfileResponse!\n}\n\ntype User {\n  id: Int!\n  firstName: String!\n  lastName: String!\n  fullName: String\n  profilePhotoUrl: String\n  phoneNumber: String!\n  verifiedPhoneNumber: Boolean!\n  gender: String!\n  birthDate: String!\n  job: String!\n  password: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignUpResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype StartPhoneVerificationResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  verified: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n"];
+export const typeDefs = ["type CreateInterestsResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Mutation {\n  CreateInterests(name: String!): CreateInterestsResponse\n  CompletPhoneNumberVerification(phoneNumber: String!, key: String!): CompletPhoneNumberVerificationResponse!\n  SignUp(firstName: String!, lastName: String!, profilePhotoUrl: String!, phoneNumber: String!, gender: String!, birthDate: String!, job: String!, password: String!): SignUpResponse!\n  StartPhoneVerification(phoneNumber: String!): StartPhoneVerificationResponse!\n}\n\ntype Interests {\n  id: Int!\n  name: String!\n  user: User\n  createdAt: String!\n  updatedAt: String\n}\n\ntype CompletPhoneNumberVerificationResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype GetUserProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype Query {\n  GetUserProfile: GetUserProfileResponse!\n}\n\ntype User {\n  id: Int!\n  firstName: String!\n  lastName: String!\n  fullName: String\n  profilePhotoUrl: String\n  phoneNumber: String!\n  verifiedPhoneNumber: Boolean!\n  gender: String!\n  birthDate: String!\n  job: String!\n  password: String!\n  interests: [Interests]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignUpResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype StartPhoneVerificationResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  verified: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -23,14 +23,28 @@ export interface User {
   birthDate: string;
   job: string;
   password: string;
+  interests: Array<Interests> | null;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Interests {
+  id: number;
+  name: string;
+  user: User | null;
   createdAt: string;
   updatedAt: string | null;
 }
 
 export interface Mutation {
+  CreateInterests: CreateInterestsResponse | null;
   CompletPhoneNumberVerification: CompletPhoneNumberVerificationResponse;
   SignUp: SignUpResponse;
   StartPhoneVerification: StartPhoneVerificationResponse;
+}
+
+export interface CreateInterestsMutationArgs {
+  name: string;
 }
 
 export interface CompletPhoneNumberVerificationMutationArgs {
@@ -51,6 +65,11 @@ export interface SignUpMutationArgs {
 
 export interface StartPhoneVerificationMutationArgs {
   phoneNumber: string;
+}
+
+export interface CreateInterestsResponse {
+  ok: boolean;
+  error: string | null;
 }
 
 export interface CompletPhoneNumberVerificationResponse {
