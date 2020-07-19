5d9d4bd1f93b0cf4d586c0e7bfcc6f5ec6bea3dc
alswnd <alswnd0427@gmail.com>
2020-07-19 15:45:51 +0900

    GetUserProfile resolver

---
+++ src/api/User/GetUserProfile/GetUserProfile.resolvers.ts
@@ -0,0 +1,24 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetUserProfile: privateResolver(async (_res, _args, { req }) => {
+      const { user } = req;
+
+      if (user) {
+        return {
+          ok: true,
+          error: null,
+          user,
+        };
+      } else {
+        return {
+          ok: true,
+          error: "user-not-found",
+          user: null,
+        };
+      }
+    }),
+  },
+};
