4c3ec7b34f9edc4f7e3f39812f248c09be3bfc5e
alswnd <alswnd0427@gmail.com>
2020-07-21 22:53:04 +0900

    trying payment approve

--- src/utils/ready.payment.ts
+++ src/utils/ready.payment.ts
@@ -3,7 +3,7 @@ import request from "request";
 
 export const readyPayment = async () => {
   const adminKey = process.env.KP_ADMIN_KEY;
-  const CID = process.env.KP_ONETIME_CID;
+  const CID = process.env.KP_SUBSCRIP_CID;
 
   const options = {
     uri: "https://kapi.kakao.com/v1/payment/ready",
@@ -13,8 +13,8 @@ export const readyPayment = async () => {
       partner_order_id: "1234",
       partner_user_id: "1234",
       item_name: "test",
-      quantity: 1,
-      total_amount: 1,
+      quantity: 0,
+      total_amount: 0,
       tax_free_amount: 0,
       approval_url: "https://koreablockchainweek.com",
       cancel_url: "https://koreablockchainweek.com",
@@ -27,25 +27,49 @@ export const readyPayment = async () => {
     },
   };
 
-  // {
-  //   tid: "T2786082956768948836",
+  // const res = {
+  //   tid: "T2786084756360245862",
   //   tms_result: false,
   //   next_redirect_app_url:
-  //     "https://mockup-pg-web.kakao.com/v1/6fa466eeec5b4893a65010b27fbe1cb9b226bda684c32b6e526109d7df12154d/aInfo",
+  //     "https://mockup-pg-web.kakao.com/v1/c6550a013757a79e94e1275254e943b22c485bef10eb0bdf3510f17783f503d7/aInfo",
   //   next_redirect_mobile_url:
-  //     "https://mockup-pg-web.kakao.com/v1/6fa466eeec5b4893a65010b27fbe1cb9b226bda684c32b6e526109d7df12154d/mInfo",
+  //     "https://mockup-pg-web.kakao.com/v1/c6550a013757a79e94e1275254e943b22c485bef10eb0bdf3510f17783f503d7/mInfo",
   //   next_redirect_pc_url:
-  //     "https://mockup-pg-web.kakao.com/v1/6fa466eeec5b4893a65010b27fbe1cb9b226bda684c32b6e526109d7df12154d/info",
+  //     "https://mockup-pg-web.kakao.com/v1/c6550a013757a79e94e1275254e943b22c485bef10eb0bdf3510f17783f503d7/info",
   //   android_app_scheme:
-  //     "kakaotalk://kakaopay/pg?url=https://mockup-pg-web.kakao.com/v1/6fa466eeec5b4893a65010b27fbe1cb9b226bda684c32b6e526109d7df12154d/order",
+  //     "kakaotalk://kakaopay/pg?url=https://mockup-pg-web.kakao.com/v1/c6550a013757a79e94e1275254e943b22c485bef10eb0bdf3510f17783f503d7/order",
   //   ios_app_scheme:
-  //     "kakaotalk://kakaopay/pg?url=https://mockup-pg-web.kakao.com/v1/6fa466eeec5b4893a65010b27fbe1cb9b226bda684c32b6e526109d7df12154d/order",
-  //   created_at: "2020-07-21T22:24:48",
+  //     "kakaotalk://kakaopay/pg?url=https://mockup-pg-web.kakao.com/v1/c6550a013757a79e94e1275254e943b22c485bef10eb0bdf3510f17783f503d7/order",
+  //   created_at: "2020-07-21T22:31:47",
   // };
 
   request.post(options, (err, res) => {
-    console.log("\n\n\n\n\n\n######", err, res.body, "\n\n\n\n\n");
-    if (err !== null) return err;
-    else return res;
+    console.log("\n\n\n\n\n\n", err, res.body, "\n\n\n\n\n");
+
+    const resBody = JSON.parse(res.body);
+    const { tid, next_redirect_app_url } = resBody;
+
+    const pg_token = next_redirect_app_url.split("/")[4];
+
+    const approve_options = {
+      uri: "https://kapi.kakao.com/v1/payment/approve",
+      method: "POST",
+      form: {
+        cid: CID,
+        tid,
+        partner_order_id: "1234",
+        partner_user_id: "1234",
+        pg_token,
+      },
+
+      headers: {
+        Host: "kapi.kakao.com",
+        Authorization: "KakaoAK " + adminKey,
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    };
+    request.post(approve_options, (err, res) => {
+      console.log("\n\n\n\n\n\n", err, res.body, "\n\n\n\n\n");
+    });
   });
 };
