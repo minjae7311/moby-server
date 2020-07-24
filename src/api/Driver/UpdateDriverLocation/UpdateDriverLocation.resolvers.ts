1fec04492cc0eb7cd1c15effa6646ccbb4f5c7c8
alswnd <alswnd0427@gmail.com>
2020-07-24 22:00:35 +0900

    Update driver location

---
+++ src/api/Driver/UpdateDriverLocation/UpdateDriverLocation.resolvers.ts
@@ -0,0 +1,45 @@
+import { Resolvers } from "../../../types/resolvers";
+import Driver from "../../../entities/Driver";
+import {
+  UpdateDriverLocationMutationArgs,
+  UpdateDriverLocationResponse,
+} from "../../../types/graph";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateDriverLocation: async (
+      _res,
+      args: UpdateDriverLocationMutationArgs,
+      _req
+    ): Promise<UpdateDriverLocationResponse> => {
+      const driver = await Driver.findOne({
+        id: args.driverId.id,
+      });
+
+      if (!driver) {
+        return {
+          ok: false,
+          error: "driver-not-found",
+        };
+      } else {
+        try {
+          driver.lat = args.lat;
+          driver.lng = args.lng;
+          await driver.save();
+
+          return {
+            ok: true,
+            error: null,
+          };
+        } catch (e) {
+          return {
+            ok: false,
+            error: e.message,
+          };
+        }
+      }
+    },
+  },
+};
+
+export default resolvers;
