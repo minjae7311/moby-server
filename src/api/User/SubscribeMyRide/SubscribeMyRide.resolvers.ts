98bc1440b23b270311b33bf40e2199706d701123
alswnd <alswnd0427@gmail.com>
2020-07-25 17:54:14 +0900

    Pick up passenger

--- src/api/User/SubscribeMyRide/SubscribeMyRide.resolvers.ts
+++ src/api/User/SubscribeMyRide/SubscribeMyRide.resolvers.ts
@@ -6,6 +6,9 @@ const resolvers = {
       subscribe: withFilter(
         (_res, _args, { pubSub }) => pubSub.asyncIterator("rideStatusUpdating"),
         async (payload, __args, { context }) => {
+          /**
+           * @todo 현재 요청한 라이드를 특정할 더 효과적인 방법?
+           */
           const { currentUser } = context;
 
           const {
