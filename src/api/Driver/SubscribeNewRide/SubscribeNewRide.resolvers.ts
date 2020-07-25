3b96c30aee77df90c068802a1e1d634127de1386
alswnd <alswnd0427@gmail.com>
2020-07-25 16:34:00 +0900

    Driver location update with reload()

--- src/api/Driver/SubscribeNewRide/SubscribeNewRide.resolvers.ts
+++ src/api/Driver/SubscribeNewRide/SubscribeNewRide.resolvers.ts
@@ -1,23 +1,21 @@
 import { withFilter } from "graphql-yoga";
-// import Driver from "../../../entities/Driver";
+import Driver from "../../../entities/Driver";
 
 const resolvers = {
   Subscription: {
-    Subscribing: {
+    SubscribeNewRide: {
       subscribe: withFilter(
-        (_res, _args, { pubSub }) =>
-          pubSub.asyncIterator(["rideRequesting", "driverLocationUpdating"]),
+        (_res, _args, { pubSub }) => pubSub.asyncIterator("rideRequesting"),
         async (payload, __args, { context }) => {
           console.log(payload);
 
-          // const currentDriver: Driver = context.currentDriver;
+          const currentDriver: Driver = context.currentDriver;
           const {
             SubscribeNewRide: { from },
-            SubscribeNewDriverLocation: { lat, lng },
           } = payload;
 
-          // await currentDriver.reload();
-          // const { lat, lng } = currentDriver;
+          await currentDriver.reload();
+          const { lat, lng } = currentDriver;
 
           return (
             from.lat >= lat - 0.05 &&
