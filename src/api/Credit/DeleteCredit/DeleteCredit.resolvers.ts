018b5e98e3cbaa76a22c60aa31ebdc6ce95b0b9c
alswnd <alswnd0427@gmail.com>
2020-08-04 00:10:37 +0900

    Delete Credit

---
+++ src/api/Credit/DeleteCredit/DeleteCredit.resolvers.ts
@@ -0,0 +1,56 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import {
+  DeleteCreditMutationArgs,
+  DeleteCreditResponse,
+} from "../../../types/graph";
+import Credit from "../../../entities/Credit";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    DeleteCredit: privateResolver(
+      async (
+        _res,
+        args: DeleteCreditMutationArgs,
+        { req }
+      ): Promise<DeleteCreditResponse> => {
+        const { user } = req;
+        const { creditId } = args;
+
+        const credit = await Credit.findOne(
+          { id: creditId },
+          { relations: ["user"] }
+        );
+
+        if (!credit) {
+          return {
+            ok: false,
+            error: "credit-not-found",
+          };
+        }
+
+        if (credit.user.id != user.id) {
+          return {
+            ok: false,
+            error: "un-auth-to-this-credit",
+          };
+        }
+
+        try {
+          await credit.remove();
+          return {
+            ok: true,
+            error: null,
+          };
+        } catch (e) {
+          return {
+            ok: false,
+            error: e.message,
+          };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
