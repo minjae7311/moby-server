8dfed47fe34493ec4b928bfbdd605cc149bafd45
alswnd <alswnd0427@gmail.com>
2020-08-08 22:12:54 +0900

    handleFindingDistance 고민고민

---
+++ src/utils/handleFindingDistance.ts
@@ -0,0 +1,9 @@
+import Ride from "../entities/Ride";
+
+export const handleFindingDistance = (ride: Ride) => {
+    /**
+     * @todo sleep, and reload ride 
+     *       if ride is still requesting,
+     *       then findDistance += 10 and resend to pubSub channel..
+     */
+};
