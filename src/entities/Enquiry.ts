d61b1cf41e0e8b2a831872ec338855af838a6ea2
alswnd <alswnd0427@gmail.com>
2020-08-19 16:50:26 +0900

    Enquiry entity & EnrollEnquiry

---
+++ src/entities/Enquiry.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from "typeorm";
+import User from "./User";
+
+@Entity()
+class Enquiry extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne((type) => User, { nullable: false })
+  user: string;
+
+  @Column({ type: "text", nullable: false })
+  questionTitle: string;
+
+  @Column({ type: "text", nullable: false })
+  questionContent: string;
+
+  @Column({ type: "text", nullable: true })
+  answerTitle: string;
+
+  @Column({ type: "text", nullable: true })
+  answerContent: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @CreateDateColumn()
+  updatedAt: string;
+}
+
+export default Enquiry;
