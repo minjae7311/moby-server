e9e86ab1d61f9a7932826bdd9bfc622545e949b0
alswnd <alswnd0427@gmail.com>
2020-08-23 14:41:50 +0900

    GetAllInterests

---
+++ src/api/User/GetAllInterests/GetAllInterests.resolvers.ts
@@ -0,0 +1,32 @@
+import { Resolvers } from "../../../types/resolvers";
+import Interests from "../../../entities/Interests";
+import { GetAllInterestsResponse } from "../../../types/graph";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetAllInterests: async (
+      _res,
+      _args,
+      _
+    ): Promise<GetAllInterestsResponse> => {
+      try {
+        const interests = await Interests.find();
+
+        console.log(interests);
+        return {
+          ok: true,
+          error: null,
+          interests,
+        };
+      } catch (e) {
+        return {
+          ok: false,
+          error: e.message,
+          interests: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
