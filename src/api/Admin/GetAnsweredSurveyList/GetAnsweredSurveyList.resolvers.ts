a4c08b8416da03b7d7ce22450253a6d786c123c2
alswnd <alswnd0427@gmail.com>
2020-09-13 21:56:26 +0900

    delete take, page in answeredsurvyelist

--- src/api/Admin/GetAnsweredSurveyList/GetAnsweredSurveyList.resolvers.ts
+++ src/api/Admin/GetAnsweredSurveyList/GetAnsweredSurveyList.resolvers.ts
@@ -1,51 +1,32 @@
 import { Resolvers } from "../../../types/resolvers";
 import adminPrivateResolvers from "../../../utils/adminPrivateResolvers";
-import {
-  GetAnsweredSurveyListQueryArgs,
-  GetAnsweredSurveyListResponse,
-} from "../../../types/graph";
+import { GetAnsweredSurveyListResponse } from "../../../types/graph";
 import SurveyAnswered from "../../../entities/SurveyAnswered";
 
 const resolvers: Resolvers = {
-  Query: {
-    GetAnsweredSurveyList: adminPrivateResolvers(
-      async (
-        _res,
-        args: GetAnsweredSurveyListQueryArgs,
-        { req }
-      ): Promise<GetAnsweredSurveyListResponse> => {
-        const { take, page } = args;
+	Query: {
+		GetAnsweredSurveyList: adminPrivateResolvers(
+			async (_res, _args, { req }): Promise<GetAnsweredSurveyListResponse> => {
+				try {
+					const answeredSurvey = await SurveyAnswered.find({
+						relations: ["user", "ride", "surveyForm", "ride.vehicle", "ride.from", "ride.to", "ride.payment"],
+					});
 
-        try {
-          const answeredSurvey = await SurveyAnswered.find({
-            skip: (page - 1) * take,
-            take,
-            relations: [
-              "user",
-              "ride",
-              "surveyForm",
-              "ride.vehicle",
-              "ride.from",
-              "ride.to",
-              "ride.payment",
-            ],
-          });
-
-          return {
-            ok: true,
-            error: null,
-            answeredSurvey,
-          };
-        } catch (e) {
-          return {
-            ok: false,
-            error: e.message,
-            answeredSurvey: null,
-          };
-        }
-      }
-    ),
-  },
+					return {
+						ok: true,
+						error: null,
+						answeredSurvey,
+					};
+				} catch (e) {
+					return {
+						ok: false,
+						error: e.message,
+						answeredSurvey: null,
+					};
+				}
+			}
+		),
+	},
 };
 
 export default resolvers;
